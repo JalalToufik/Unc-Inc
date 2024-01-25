1ac4b45c475782f9d5bdc6adb7996b0c8f45acbd
JalalToufik <112856590+JalalToufik@users.noreply.github.com>
2024-01-25 07:26:56 +0100

    formulier kan inloggen

--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const [userData, setUserData] = useState({
+        username: 'uncinc',
+        password: 'letmein'
+    });
+
+    const [errorMessages, setErrorMessages] = useState({});
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const { uname, pass } = event.target;
+
+        if (userData.username === uname.value && userData.password === pass.value) {
+        // Successful login
+        setUserData({ username: uname.value, password: pass.value });
+        // Navigate to '/dashboard'
+        window.location.href = '/dashboard';
+        } else {
+        // Invalid login credentials
+        setErrorMessages({
+            uname: 'Invalid username',
+            pass: 'Invalid password'
+        });
+        }
+    };
+
+    const renderErrorMessage = (name) =>
+        name === errorMessages.name && (
+        <div className="error">{errorMessages.message}</div>
+        );
+
     return (
+        <main>
         <section className='login-container'>
-            <h1>Welcome</h1>
-            <form>
-                <label>
-                    Gebruikersnaam:
-                    <input type="text" required/>
-                </label>
-                <label>
-                    Wachtwoord:
-                    <input type="password" required/>
-                </label>
-                <button type="submit">Confirm</button>
+            <h1>Welcome back!</h1>
+            <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text" name="uname" placeholder="Enter your username" required/>
+                {renderErrorMessage("uname")}
+            </label>
+            <label>
+                Password:
+                <input type="password" name="pass" placeholder="Enter your password" required />
+                {renderErrorMessage("pass")}
+            </label>
+            <button type="submit">Login</button>
+            {userData.username && userData.password && <Link to="/dashboard">Dashboard</Link>}
             </form>
         </section>
-    )
-}
+        </main>
+    );
+};
 
-export default Login
-
+export default Login;
--- src/Navbar.js
+++ src/Navbar.js
@@ -10,6 +10,9 @@ const Navbar = () => {
             <NavLink to="/home"> 
                 <h1>Home</h1>
             </NavLink>
+            <NavLink to="/dashboard"> 
+                Dashboard
+            </NavLink>
             <NavLink to="/login"> 
                 Login
             </NavLink>
